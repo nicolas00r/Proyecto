e538f1fa2652c9cd06739f2660930706c2e0d7b5
intitix <163463902+intitix@users.noreply.github.com>
2024-06-19 01:20:36 +0000

    Prueba

--- .ccls-cache/@home@runner@Proyecto/main.c
+++ .ccls-cache/@home@runner@Proyecto/main.c
@@ -3,34 +3,32 @@
 
 //#include "tdas/queue.h"
 
-//STRUCTS
-// Declarar los datos de los jugadores
+// STRUCTS
+//  Declarar los datos de los jugadores
 typedef struct {
-    int dinero;                    // Dinero del jugador
-    int penalizacion;              // Penalizacion de la carcel hacia al jugador
-    char nombre_jugador[40];       // Nombre del jugador
-    int posicion;                  // Posicion del jugador
-//    List *propiedades;    
-//    List *cartas;
-}TipoJugador;
-
-
-typedef struct {
-    char nombre_casilla[40];       // Nombre de la casilla
-    char tipo[5];                 // Tipo de casilla "P" para propiedad y "C" para carta
-    union {
-       // TipoPropiedad *propiedad;    
-        int cantidad_impuesto;
-        //TipoCarta* carta;              
-    };
-}TipoCasilla;
-
+  int dinero;              // Dinero del jugador
+  int penalizacion;        // Penalizacion de la carcel hacia al jugador
+  char nombre_jugador[40]; // Nombre del jugador
+  int posicion;            // Posicion del jugador
+  //    List *propiedades;
+  //    List *cartas;
+} TipoJugador;
 
 typedef struct {
-     TipoCasilla *casillas[40]; // Arreglo de casillas del tablero
-}TipoTablero;
+  char nombre_casilla[40]; // Nombre de la casilla
+  char tipo[5]; // Tipo de casilla "P" para propiedad y "C" para carta
+  union {
+    // TipoPropiedad *propiedad;
+    int cantidad_impuesto;
+    // TipoCarta* carta;
+  };
+} TipoCasilla;
+
+typedef struct {
+  TipoCasilla *casillas[40]; // Arreglo de casillas del tablero
+} TipoTablero;
 
 int main(void) {
-  printf("Hello World\n");
+  printf("HOLA MUNDO ESTO ES UN CAMBIO\n");
   return 0;
 }
