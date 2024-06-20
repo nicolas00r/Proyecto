b7f222feec0299cc351b0b25d2eb155f11928506
nicolas00r <163439327+nicolas00r@users.noreply.github.com>
2024-06-20 22:23:49 +0000

    Creación de archivo funciones.c y header

---
+++ .ccls-cache/@home@runner@Proyecto/funciones.h
@@ -0,0 +1,22 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+typedef struct TipoPropiedad TipoPropiedad;
+typedef struct TipoJugador TipoJugador;
+typedef struct partidaGlobal partidaGlobal;
+
+// Declaraciones de funciones
+void presioneEnter();
+void limpiar_pantalla();
+void inicializar_aleatoriedad();
+int tirar_dados();
+TipoPropiedad* inicializar_propiedades();
+TipoPropiedad* buscar_propiedad_por_nombre(TipoPropiedad* propiedades, const char* nombre);
+void comprar_propiedad(TipoJugador* jugador, TipoPropiedad* propiedad);
+void comprar_casas(TipoJugador* jugador, TipoPropiedad* propiedad);
+void testear_funciones();
+void mostrar_reglas();
+void guardar_partida(partidaGlobal *partida, const char *filename);
+void mostrarMenuInicial();
+
+#endif // FUNCIONES_H
